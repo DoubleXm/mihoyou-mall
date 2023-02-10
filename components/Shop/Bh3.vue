6b353e861151376dc2f5dc44050e94cddcb8299b
xsq <xu18516321273@qq.com>
2023-02-10 23:06:39 +0800

    style: 测试提交

--- components/Shop/Bh3.vue
+++ components/Shop/Bh3.vue
@@ -1,12 +1,24 @@
 <script lang="ts">
+import { shopConfig } from '~/settings/static'
+
 export default defineComponent({
   name: 'ShopBh3',
   setup() {
-    return {}
+    return {
+      shopConfig,
+    }
   },
 })
 </script>
 
 <template>
-  <div>崩坏3</div>
+  <div class="wd-wrapper">
+    <div class="banner">
+      <img :src="shopConfig.wd.banner" alt="banner">
+    </div>
+
+    <div class="content-1" />
+    <div class="content-2" />
+    <div class="content-3" />
+  </div>
 </template>
